2d3097822f28c52c234d557bfd3518ff605590d0
toddync <dagvjr@gmail.com>
2024-05-19 11:28:11 -0300

    fix recursive connection issue

--- src/lib/components/nodes/Handle.svelte
+++ src/lib/components/nodes/Handle.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
 	//@ts-nocheck
 	import { Handle } from "@xyflow/svelte";
+	const validate = (c) => {
+		const getGroup = (gId) => {
+			let _ = (gId as String).split("---");
+
+			if (typeof _ == "object") {
+				if (_?.length > 1) {
+					_.pop();
+					console.log(_);
+					return _.join("---");
+				} else {
+					return _.pop();
+				}
+			}
+
+			return _;
+		};
+
+		if (c.target.includes("---") || c.source.includes("---")) {
+			const SourceGroup = getGroup(c.source);
+			const TargetGroup = getGroup(c.target);
+
+			return SourceGroup == TargetGroup;
+		} else {
+			return true;
+		}
+	};
 </script>
 
 <div
@@ -11,13 +37,7 @@
 		position={$$props.position}
 		type={$$props.type}
 		id={$$props.id}
-		isValidConnection={$$props.isValidConnection ||
-			((c) =>
-				($$props.type == "source"
-					? !c.target.includes("---")
-					: !c.source.includes("---")) &&
-				c.targetHandle != "start" &&
-				c.sourceHandle != "start")}
+		isValidConnection={$$props.isValidConnection || validate}
 		class="bg-transparent rounded-sm z-50 w-full h-full -translate-y-1/2 top-1/2 -translate-x-1/2 -right-1/2 {$$props.handleClass}"
 	/>
 </div>
