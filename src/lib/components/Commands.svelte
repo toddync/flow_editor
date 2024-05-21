7c135cc833c05b99aced0646b4ab535143caf757
toddync <dagvjr@gmail.com>
2024-05-21 20:38:18 -0300

    ui rework for buttons

--- src/lib/components/Commands.svelte
+++ src/lib/components/Commands.svelte
@@ -1,21 +1,36 @@
 <script lang="ts">
 	//@ts-nocheck
-	import { onMount } from "svelte";
-	import { Blocks as Node } from "lucide-svelte/icons";
-	import { showCommands as open } from "$lib/stores/ShowCommands.js";
 	import * as Command from "$lib/components/ui/command/index.js";
+	import { showCommands as open } from "$lib/stores/ShowCommands.js";
+	import {
+		Code2,
+		CurlyBraces,
+		Image,
+		Blocks as Node,
+	} from "lucide-svelte/icons";
+	import { onMount } from "svelte";
 
-	import { Nodes } from "$lib/stores/nodesStore";
-	import { useSvelteFlow } from "@xyflow/svelte";
+	import { Edges } from "$lib/stores/edgesStore";
 	import { nodeTypes } from "$lib/stores/nodeTypes";
+	import { Nodes } from "$lib/stores/nodesStore";
+	import { Transpile } from "$lib/transpiler";
+	import {
+		getNodesBounds,
+		getViewportForBounds,
+		useNodes,
+		useSvelteFlow,
+	} from "@xyflow/svelte";
+	import { toSvg } from "html-to-image";
 
 	const { screenToFlowPosition } = useSvelteFlow();
 	const types = Object.keys($nodeTypes);
 
+	const close = () => ($open = !$open);
+
 	const addNode = (type) => {
 		let { x, y } = screenToFlowPosition({
 			x: window.innerWidth / 2,
-			y: window.innerHeight / 2
+			y: window.innerHeight / 2,
 		});
 
 		$Nodes.push({
@@ -23,12 +38,87 @@
 			position: { x, y },
 			type: type,
 			dragHandle: type.toLowerCase().includes("var") && ".drag",
-			data: {}
+			data: {},
 		});
 
 		$Nodes = $Nodes;
-		$open = !$open;
+		close();
 	};
+
+	const nodes = useNodes();
+	export function ImageDown() {
+		const scale = 2;
+		let imageWidth = 1024 * scale;
+		let imageHeight = 768 * scale;
+
+		const nodesBounds = getNodesBounds($nodes);
+		const viewport = getViewportForBounds(
+			nodesBounds,
+			imageWidth,
+			imageHeight,
+			0.5,
+			2.0,
+			0.9,
+		);
+
+		const viewportDomNode = document.querySelector(
+			".svelte-flow__viewport",
+		)!;
+
+		if (viewport) {
+			toSvg(viewportDomNode, {
+				width: imageWidth,
+				height: imageHeight,
+				style: {
+					width: `${imageWidth}px`,
+					height: `${imageHeight}px`,
+					position: "absolute",
+					transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`,
+					// "background-image": `url("http://localhost:5173/back.png")`,
+				},
+			}).then((dataUrl) => {
+				download(dataUrl, "svelte-flow.svg");
+			});
+		}
+	}
+
+	function CodeDown() {
+		download(
+			"data:text/plain;charset=utf-8," +
+				encodeURIComponent(Transpile($Nodes, $Edges)),
+			"svelte-flow.js",
+		);
+	}
+
+	function JsonDown() {
+		download(
+			"data:text/plain;charset=utf-8," +
+				encodeURIComponent(
+					JSON.stringify({ Nodes: $Nodes, Edges: $Edges }, null, 2),
+				),
+			"svelte-flow.json",
+		);
+	}
+
+	function restore(e) {
+		const reader = new FileReader();
+		reader.readAsText(e.target.files[0]);
+		reader.onload = function () {
+			let data = JSON.parse(reader.result);
+
+			$Nodes = data.Nodes;
+			$Edges = data.Edges;
+			close();
+		};
+	}
+
+	function download(ref, name) {
+		let _ = document.createElement("a");
+		_.setAttribute("href", ref);
+		_.setAttribute("download", name);
+		_.click();
+		close();
+	}
 
 	onMount(() => {
 		function handleKeydown(e) {
@@ -49,7 +139,7 @@
 	<Command.Input placeholder="Type a command or search..." />
 	<Command.List>
 		<Command.Empty>No results found.</Command.Empty>
-		<Command.Group heading="Suggestions">
+		<Command.Group heading="Nodes">
 			{#each types as type}
 				{#if type != "StartNode"}
 					<Command.Item onSelect={() => addNode(type)}>
@@ -59,6 +149,37 @@
 				{/if}
 			{/each}
 		</Command.Group>
-		<!-- <Command.Separator /> -->
+		<Command.Separator />
+		<Command.Group heading="Exports">
+			<Command.Item onSelect={ImageDown}>
+				<Image class="mr-2 h-4 w-4" />
+				<span>Download Flow as SVG</span>
+			</Command.Item>
+			<Command.Item onSelect={CodeDown}>
+				<Code2 class="mr-2 h-4 w-4" />
+				<span>Download Flow as Code</span>
+			</Command.Item>
+			<Command.Item onSelect={JsonDown}>
+				<CurlyBraces class="mr-2 h-4 w-4" />
+				<span>Download Flow as Json</span>
+			</Command.Item>
+		</Command.Group>
+		<Command.Separator />
+		<Command.Group heading="Import">
+			<Command.Item
+				onSelect={() => document.querySelector("#json").click()}
+			>
+				<CurlyBraces class="mr-2 h-4 w-4" />
+				<span>Upload Flow from Json</span>
+			</Command.Item>
+		</Command.Group>
 	</Command.List>
 </Command.Dialog>
+
+<input
+	type="file"
+	class="sr-only"
+	id="json"
+	accept=".json"
+	on:change={restore}
+/>
--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
 	//@ts-nocheck
 	import { Panel } from "@xyflow/svelte";
-	import ImageDownload from "./ImageDownload.svelte";
-	import JsonDownload from "./JsonDownload.svelte";
-	import CodeDownload from "./CodeDownload.svelte";
+	import { Button } from "./ui/button";
+	import { Command, Search } from "lucide-svelte";
+	import { Badge } from "./ui/badge";
+	import { showCommands as open } from "$lib/stores/ShowCommands";
 </script>
 
-<Panel position="top-right">
-	<ImageDownload />
-	<JsonDownload />
-	<CodeDownload />
+<Panel position="top-center">
+	<Button
+		on:click={() => ($open = true)}
+		class="bg-black/10 backdrop-blur-sm border hover:bg-accent rounded-2xl flex gap-5 group transition-colors"
+	>
+		<Search class="text-foreground size-5" />
+
+		<p class="text-muted-foreground mx-auto col-span-2">Search...</p>
+
+		<Badge
+			variant="secondary"
+			class="text-muted-foreground group-hover:bg-red-900/50 transition-colors"
+		>
+			<Command class="size-4" />
+			+ K
+		</Badge>
+	</Button>
 </Panel>
